52277b87ed6b09f92562a7976ce6fd62da89968a
Paul <p.aul.jarvey@gmail.com>
2017-07-25 23:38:14 -0400

    Completed assignment

--- README.md
+++ README.md
@@ -103,3 +103,12 @@ In order to complete this assignment, you must do the following:
 ### Grading
 
 This assignment will be graded via peer assessment.
+
+
+
+
+
+
+
+
+
--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions below create a cache of the inverse of matrix objects,
+## and use cached values to calculate matrix inverses with less total computation time.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  # Setup a variable to hold cached values, initiate as NULL
+  cache <- NULL
+  
+  # set a matrix
+  setMatrix <- function(y) {
+    x <<- y
+    # empty the cache
+    cache <<- NULL
+  }
+  
+  # return the stored matrix
+  getMatrix <- function() {
+    x
+  }
+  
+  # calculate the inverse of the matrix, cache the result
+  setInverse <- function(solve) {
+    cache <<- solve
+  }
+  
+  # get the cached inverse
+  getInverse <- function(){
+    cache
+  }
+  
+  #return a list, composed of function for getting/caching 
+  list(setMatrix = setMatrix, 
+       getMatrix = getMatrix,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special
+## "matrix" returned by `makeCacheMatrix` above. If the inverse has
+## already been calculated (and the matrix has not changed), then
+##`cacheSolve` will retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  cache <- x$getInverse()
+  if(!is.null(cache)) {
+    message("getting cached data")
+    return(cache)
+  }
+  data <- x$getMatrix()
+  cache <- solve(data, ...)
+  x$setInverse(cache)
+  cache
 }
